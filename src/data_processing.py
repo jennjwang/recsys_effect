77ec65700a95b42e008b8abe0f0c2d7f14573795
Jennifer Wang <38547472+jennjwang@users.noreply.github.com>
2023-12-28 18:36:01 -0500

    more more computing for % of users w shared history

--- src/data_processing.py
+++ src/data_processing.py
@@ -3,6 +3,7 @@ import networkx as nx
 import dask.dataframe as dd
 import dask.distributed
 import numpy as np
+from itertools import combinations
 import matplotlib.pyplot as plt
 
 from scipy.sparse import csr_matrix
@@ -46,9 +47,9 @@ behaviors_df['impressions'] = behaviors_df['impressions'].str.split(' ')
 # get the number of items recommended to users
 behaviors_df['impressions'] = behaviors_df['impressions'].apply(lambda x: [item for item in x if not item.endswith('0')])
 print(behaviors_df.head())
-behaviors_df['empty'] = behaviors_df['impressions'].apply(lambda x: x == [])
-print('users', behaviors_df['user_id'].nunique())
-print('users without interaction: ', behaviors_df[behaviors_df['empty']]['user_id'].nunique())
+# behaviors_df['empty'] = behaviors_df['impressions'].apply(lambda x: x == [])
+print('number of users', behaviors_df['user_id'].nunique())
+# print('users without interaction: ', behaviors_df[behaviors_df['empty']]['user_id'].nunique())
 
 # behaviors_df['impressions_len'] = behaviors_df['impressions'].apply(len)
 # print(behaviors_df['impressions_len'].median()) # 24, the # of items users actually interacted with: 1
@@ -57,6 +58,18 @@ print('users without interaction: ', behaviors_df[behaviors_df['empty']]['user_i
 # print(behaviors_df['impressions_len'].max()) # 299, the # of items users actually interacted with: 35
 # Explode the 'impressions' column into separate rows
 behaviors_df = behaviors_df.explode('impressions', ignore_index=True)
+impression_counts = behaviors_df.groupby('impressions')['user_id'].nunique()
+num_shared_impressions = (impression_counts > 1).sum()
+print('num_shared', num_shared_impressions)
+
+impression_users = behaviors_df.groupby('impressions')['user_id'].unique()
+user_pairs = impression_users.apply(lambda x: list(combinations(x, 2)))
+# Flatten the list of user pairs and convert to a set to get the unique pairs
+unique_user_pairs = set(pair for pairs_list in user_pairs for pair in pairs_list)
+# Count the number of unique pairs
+num_unique_pairs = len(unique_user_pairs)
+print("num_unique", num_unique_pairs) # 69699419 / 1249975000 (from 50,000 C 2) = 0.0557
+
 print(behaviors_df.shape) #(5843442, 2)
 # filter out the items the user didn't interact with
 # behaviors_df = behaviors_df[~behaviors_df['impressions'].str.endswith('0')]
@@ -64,10 +77,11 @@ print(behaviors_df.shape) #(5843442, 2)
 behaviors_df['impressions'] = behaviors_df['impressions'].apply(lambda cell: cell[:-2])
 behaviors_df = behaviors_df.rename(columns={'impressions':'news_id'})
 # disregard if user clicks multiple times on the same item
+print('dups', behaviors_df.duplicated().sum())
 behaviors_df.drop_duplicates(inplace=True)
 
 # print(len(behaviors_df))
-print('news id', behaviors_df['news_id'].nunique()) # 20288 total, 7713 that users clicked on
+print('news ids', behaviors_df['news_id'].nunique()) # 20288 total, 7713 that users clicked on
 
 '''
 create pivot table with users as rows and new items as columns
